257a81c7ee6baf5dcc1d0d98b6864e24972550ba
Pialleport Lucas <lucaspialleport@protonmail.com>
2021-06-10 22:55:32 +0200

    Ajout des oppérations de base.

--- script.py
+++ script.py
@@ -1,2 +1,14 @@
-print("LP")
 
+#Oppération de base :
+def Addition(x:float, y:float):
+    return x+y
+
+def soustraction(x:float, y:float):
+    return x-y
+
+def multiplication(x:float, y:float):
+    return x*y
+
+def division(x:float, y:float):
+    if y == 0 : return "NULL : division par zero"
+    return x/y
